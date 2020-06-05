a2002af857840034e7c47f809ea9f505fb21dd35
weigangyi <1315230417@qq.com>
2020-06-05 12:48:45 +0800

    2.c

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <h1>基础布局</h1>
     <el-row>
-      <el-col :span="23"><div class="grid-content bg-purple-dark"></div></el-col>
+      <el-col :span="24"><div class="grid-content bg-purple-dark"></div></el-col>
     </el-row>
     <el-row>
       <el-col :span="12"><div class="grid-content bg-purple"></div></el-col>
@@ -53,11 +53,11 @@
     <h1>分栏偏移</h1>
     <el-row :gutter="20">
       <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-      <el-col :span="6" :offset="12"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="6" :offset="6"><div class="grid-content bg-purple"></div></el-col>
     </el-row>
     <el-row :gutter="20">
       <el-col :span="6" :push="6"><div class="grid-content bg-purple"></div></el-col>
-      <el-col :span="6" ><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
     </el-row>
     <el-row :gutter="20">
       <el-col :span="12" :pull="6"><div class="grid-content bg-purple"></div></el-col>
@@ -90,73 +90,60 @@
     </el-row>
     <h1>响应式布局</h1>
     <el-row :gutter="10">
-      <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1">
-        <div class="grid-content bg-purple"></div>
-      </el-col>
-      <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11">
-        <div class="grid-content bg-purple-light"></div>
-      </el-col>
-      <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11">
-        <div class="grid-content bg-purple"></div>
-      </el-col>
-      <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1">
-        <div class="grid-content bg-purple-light"></div>
-      </el-col>
+      <!-- eslint-disable -->
+      <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11"><div class="grid-content bg-purple-light"></div></el-col>
+      <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1"><div class="grid-content bg-purple-light"></div></el-col>
+      <!-- eslint-disable -->    
     </el-row>
     <h1>基于断点的隐藏类</h1>
     <el-row :gutter="10">
-      <el-col class="hidden-xs-only" :xs="8" :sm="6" :md="4" :lg="3" :xl="1">
-        <div class="grid-content bg-purple"></div>
-      </el-col>
+      <!-- eslint-disable -->
+      <el-col class="hidden-xs-only" :xs="8" :sm="6" :md="4" :lg="3" :xl="1"><div class="grid-content bg-purple"></div></el-col>
+      <!-- eslint-disable -->    
     </el-row>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-
   </div>
 </template>
 
 <script>
-import ElRow from '@element/Row.vue';
-import ElCol from '@element/Col.vue';
+// import ElRow from '@element/Row.js';
+// import ElCol from '@element/Col.js';
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    ElRow,
-    ElCol,
+    // ElRow,
+    // ElCol,
   },
 };
 </script>
 
 <style lang="scss">
 .el-row {
-    margin-bottom: 20px;
-    &:last-child {
-      margin-bottom: 0;
-    }
+  margin-bottom: 20px;
+  &:last-child {
+    margin-bottom: 0;
   }
-  .el-col {
-    border-radius: 4px;
-  }
-  .bg-purple-dark {
-    background: #99a9bf;
-  }
-  .bg-purple {
-    background: #d3dce6;
-  }
-  .bg-purple-light {
-    background: #e5e9f2;
-  }
-  .grid-content {
-    border-radius: 4px;
-    min-height: 36px;
-  }
-  .row-bg {
-    padding: 10px 0;
-    background-color: #f9fafc;
-  }
-
+}
+.el-col {
+  border-radius: 4px;
+}
+.bg-purple-dark {
+  background: #99a9bf;
+}
+.bg-purple {
+  background: #d3dce6;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
+}
 </style>
---
+++ src/plugins/element.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import Element from '@element';
+
+import '@/assets/theme-chalk/index.scss';
+import '@/assets/theme-chalk/display.scss';
+
+Vue.use(Element);
